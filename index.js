f91ad06f80a15684d54f087be440c686cdc2269f
anamikapandey745 <anamikak005@gmail.com>
2020-04-16 12:00:50 +0530

    Full stack intern challenge commit

---
+++ index.js
@@ -0,0 +1,20 @@
+addEventListener('fetch', event => {
+  var myurl = event.request.url;
+  if(myurl == 'https://example.com/'){
+  event.respondWith(handleRequest(event.request));
+} 
+  
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+
+async function handleRequest(request) {
+  let response = await fetch('http://cfw-takehome.developers.workers.dev/api/variants');
+  let resp =  await response.json();
+  let randomIndex = Math.floor(Math.random() * 2) ;
+  let finalResponse = resp.variants[randomIndex];
+  return Response.redirect(finalResponse);
+ 
+}
